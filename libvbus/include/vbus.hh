566a794cceadb3f39326ff60efa8469a733faafd
Gregory Haskins <ghaskins@novell.com>
2008-11-25 17:35:30 -0500

    libvbus: Add more support for iterating devices and attributes
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- libvbus/include/vbus.hh
+++ libvbus/include/vbus.hh
@@ -11,31 +11,43 @@
 
 namespace Virtual {
 
-	typedef boost::mutex                                     Mutex;
-	typedef boost::condition                                 CondVar;
-	typedef boost::detail::thread::scoped_lock<boost::mutex> Lock;
+    typedef boost::mutex                                     Mutex;
+    typedef boost::condition                                 CondVar;
+    typedef boost::detail::thread::scoped_lock<boost::mutex> Lock;
+    
+    class Bus {
+    public:
+	Bus();
+	
+	class Device {
+	    friend class Bus;
 
-	class Bus {
 	public:
-		Bus();
+	    typedef unsigned long Id;
+	    
+	    Id GetId() { return m_id; }
 
-		class Device {
-		public:
-			typedef unsigned long Id;
-		
-			Id          m_id;
-			std::string m_type;
-		};
-
-		typedef boost::shared_ptr<Device> DevicePtr;
-
+	    std::string Attr(const std::string &key);
+	    void Attr(const std::string &key,
+		      const std::string &val);
+	    
+	    
 	private:
-		typedef std::map<Device::Id, DevicePtr> DeviceMap;
-
-		Mutex m_mutex;
-		DeviceMap m_devicemap; 
+	    Device(Id id, const std::string &path);
+	    
+	    Id           m_id;
+	    std::string  m_path;
 	};
-
+	
+	typedef boost::shared_ptr<Device> DevicePtr;
+	
+    private:
+	typedef std::map<Device::Id, DevicePtr> DeviceMap;
+	
+	unsigned long m_version;
+	Mutex m_mutex;
+	DeviceMap m_devicemap; 
+    };
 };
 
 #endif // __VBUS_HH__
--- libvbus/lib/bus.cc
+++ libvbus/lib/bus.cc
@@ -4,31 +4,85 @@
 #include <fstream>
 #include <sstream>
 #include <exception>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+
+namespace fs = boost::filesystem;
 
 using namespace Virtual;
 
-static void CheckVersion()
+template <class T>
+static T CastString(const std::string &s)
 {
-	std::ifstream is("/sys/vbus/version");
-
-	if (!is.is_open())
-		throw std::runtime_error("cannot detect vbus feature in the kernel");
-
-	unsigned long version;
-
-	is >> version;
-
-	if (version != 1) {
-		std::ostringstream os;
-		
-		os << "Version " << version << " unsupported";
-		throw std::runtime_error(os.str());
-	}
+    std::istringstream is(s);
+    T v;
+    
+    is >> v;	
+    
+    return v;	
 }
 
-Bus::Bus()
+static std::string ReadAttr(const std::string &path)
 {
-	CheckVersion();
+    std::ifstream is(path.c_str());
+    std::string v;
+    
+    if (!is.is_open())
+	throw std::runtime_error("could not open " + path);
+    
+    is >> v;
+    
+    return v;
+}
 
+Bus::Bus() : m_version(CastString<unsigned long>(ReadAttr("/sys/vbus/version")))
+{
+    if (m_version != 1) {
+	std::ostringstream os;
 	
+	os << "Version " << m_version << " unsupported";
+	throw std::runtime_error(os.str());
+    }
+    
+    std::string buspath("/sys/vbus/instances/" +
+			ReadAttr("/proc/self/vbus"));
+    
+    if (!fs::exists(buspath))
+	throw std::runtime_error("no virtual-bus connected");
+    
+    fs::directory_iterator end_iter;
+    for ( fs::directory_iterator dev_iter(buspath + "/devices");
+	  dev_iter != end_iter;
+	  ++dev_iter )
+    {
+	Bus::Device::Id id =
+	    CastString<Bus::Device::Id>(dev_iter->path().leaf());
+	
+	Bus::DevicePtr dev(new Bus::Device(id, dev_iter->path().string()));
+
+	m_devicemap[id] = dev;
+    }
+    
+}
+
+Bus::Device::Device(Bus::Device::Id id, const std::string &path) :
+    m_id(id), m_path(path)
+{
+
+}
+
+std::string Bus::Device::Attr(const std::string &key)
+{
+    return ReadAttr(m_path + "/" + key);
+}
+
+void Bus::Device::Attr(const std::string &key, const std::string &val)
+{
+    std::string fqp(m_path + "/" + key);
+    std::ofstream os(fqp.c_str());
+    
+    if (!os.is_open())
+	throw std::runtime_error("could not open " + fqp);
+
+    os << val;
 }
