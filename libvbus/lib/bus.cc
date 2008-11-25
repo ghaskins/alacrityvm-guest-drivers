83cc43a9ba15f6f28283609dec1f38340686f618
Gregory Haskins <ghaskins@novell.com>
2008-11-25 10:43:24 -0500

    libvbus: Add trivial version checking to begin testing
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- libvbus/lib/bus.cc
+++ libvbus/lib/bus.cc
@@ -1,9 +1,34 @@
 
 #include <vbus.hh>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <exception>
 
 using namespace Virtual;
 
+static void CheckVersion()
+{
+	std::ifstream is("/sys/vbus/version");
+
+	if (!is.is_open())
+		throw std::runtime_error("cannot detect vbus feature in the kernel");
+
+	unsigned long version;
+
+	is >> version;
+
+	if (version != 1) {
+		std::ostringstream os;
+		
+		os << "Version " << version << " unsupported";
+		throw std::runtime_error(os.str());
+	}
+}
+
 Bus::Bus()
 {
+	CheckVersion();
+
 	
 }
